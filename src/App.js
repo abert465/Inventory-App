37401a678f9ee4b7a78afa124d749ea3e655ca41
bert_c <acampos892@gmail.com>
2017-02-17 11:41:21 -0600

    Updated JavaScript  with styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-import { Table, Breadcrumb, Button, ButtonToolbar } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
+
+
+//Firebase dependencies
 var uuid = require('uuid');
 var firebase = require('firebase');
 
@@ -19,19 +22,24 @@ var config = {
 };
 firebase.initializeApp(config);
 
+/*The constructor for a React component is called before it is mounted. 
+    When implementing the constructor for a React.Component subclass, you should call super(props) before any other statement.*/
 class App extends Component {
   constructor(props) {
     super(props);
-
-/*The constructor for a React component is called before it is mounted. 
-When implementing the constructor for a React.Component subclass, you should call super(props) before any other statement.*/
-
     this.state = {
       inventory: [],
       submitted: false,
-      edit: false
+      editMode: false,
+      editFields: []
     }
 
+    //handle Actions
+    this._editFirebaseData = this._editFirebaseData.bind(this); //this updates the firebase record
+    this._setFireBaseDataEditTable = this._setFireBaseDataEditTable.bind(this); //Sets the UUID we are going to modify
+    this._handleFirebaseFormChange = this._handleFirebaseFormChange.bind(this);
+    this._cancelFirebaseEdit = this._cancelFirebaseEdit.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   //loading information from database
@@ -40,8 +48,10 @@ When implementing the constructor for a React.Component subclass, you should cal
     this._loadFirebaseData();
   }
 
+  //loading data from firebase
   _loadFirebaseData() {
     var self = this;
+
     this.setState({ inventory: [] });
 
 
@@ -49,62 +59,139 @@ When implementing the constructor for a React.Component subclass, you should cal
     //A Reference represents a specific location in your Database and can be used for reading or writing data to that Database location.
     firebase.database().ref().once('value').then((snapshot) => {
       snapshot.forEach(function (data) {
+
         self.setState({
           inventory: self.state.inventory.concat(data.val())
         });
       });
     });
   }
+
+  //Allows us to edit the fields and set the data back to itself.
+  //It's a ReactJS requirement
+  //Here's a good reference: http://stackoverflow.com/questions/22220873/how-to-reload-input-value-in-react-javascript-with-virtual-dom
+  handleChange(event) {
+    var change = {};
+    change[event.target.name] = event.target.value;
+    //console.log("Field Updated");
+    //console.log(event.target.name);
+    this.setState({ editFields: change });
+  }
+
+  //controls our cancel button
+  _cancelFirebaseEdit(event) {
+    event.preventDefault();
+    this.setState({ editMode: false });
+
+  }
+  //controls our input on the fly
+  _handleFirebaseFormChange(event) {
+    event.preventDefault();
+    this.props.onChange(event.target.value);
+  }
+
+  _setFireBaseDataEditTable(event) {
+    event.preventDefault();
+
+    const recordId = event.target.value;
+
+    this.setState({
+      editMode: true,
+      editUUID: event.target.value,
+      editFields: []
+    });
+
+    var self = this; //We loose what this is once we go into the firebase database
+
+    //Query the firebase data
+    firebase.database().ref().child('inventory-app/').orderByChild("uuid").on('value',
+      (snapshot) => {
+        snapshot.forEach(function (child) {
+          //console.log(child.val()) // NOW THE CHILDREN PRINT IN ORDER
+          var value = child.val();
+          var name = value.inventory.name;
+          var quantity = value.inventory.quantity;
+          var description = value.inventory.description;
+          var uuid = value.inventory.uuid;
+          var editFields = {};
+
+          if (uuid === recordId) {
+            //console.log(value);
+            editFields["name"] = name;
+            editFields["quantity"] = quantity;
+            editFields["description"] = description;
+            editFields["uuid"] = uuid;
+
+            self.setState({ editFields: editFields });
+
+          }
+        });
+      }
+    )
+  }
+
+  //handles our event when we click the edit function
+  _editFirebaseData(event) {
+    event.preventDefault();
+    //Getting the values of each child type input
+    var details = {};
+    event.target.childNodes.forEach(function (el) {
+      if (el.tagName === 'INPUT') {
+        details[el.name] = el.value
+      }
+    });
+
+    //Resetting the property value
+
+    var uuid2 = details["uuid"];
+    var self = this;
+
+    //console.log("Update UUID " + uuid2);
+
+    firebase.database().ref().child('inventory-app/' + uuid2)
+      .update({ inventory: details });
+
+    this._loadFirebaseData();
+
+    this.setState({
+      editMode: false
+    });
+  }
+
   //handles our event when we click on the the delete button
   _handleOnClick(event) {
-    event.preventDefault()
-    console.log(event.target.value)
+    event.preventDefault();
+
+    //console.log(event.target.value)
     //removes one element
-    var uuid = event.target.value
-    firebase.database().ref().child('inventory-app/' + uuid).remove();
+
+    var uuid3 = event.target.value;
+
+    firebase.database().ref().child('inventory-app/' + uuid3)
+      .remove();
+
     //reloads the data from firebase
     this._loadFirebaseData();
   }
-//handles our event when we click the edit function
-    _editFirebaseData(event) { 
-    this.setState({ edit: true});
-    //edits one element
-    var uuid2 = uuid;
-firebase.database().ref().child('/inventory-app/' + uuid2)
-        .update({  });
-
-  }
-
-
 
   render() {
     var inputForm;
     var table;
     var rows;
+    var editView;
     var output;
 
-    if(this.state.edit) {
-      output = (
-        <div>
-          <form onSubmit={this.onSubmit.bind(this)}>
-            <input />
-          </form>
-        </div>
-      );
-    } else {
-
     inputForm = <div>
-      <h2>Please enter your inventory Item</h2>
-      <form onSubmit={this.onSubmit.bind(this)}>
-        <input className="bg-info" type="text" placeholder="Enter Name..." name="name" />
-        <input className="bg-info" type="text" placeholder="Enter description..." name="description" />
-        <input className="bg-info" type="text" placeholder="Enter quantity..." name="quantity" />
+      <h2 className="h2_1">Please enter your inventory Item</h2>
+      <form name="input" className="form-inline" onSubmit={this.onSubmit}>
+        <input className="form-control" type="text" placeholder="Enter Name..." name="name" />
+        <input className="form-control" type="text" placeholder="Enter description..." name="description" />
+        <input className="form-control" type="text" placeholder="Enter quantity..." name="quantity" />
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
 
     var self = this;
-
     rows = this.state.inventory.map(function (item, index) {
 
       //console.log(item);
@@ -114,11 +201,11 @@ firebase.database().ref().child('/inventory-app/' + uuid2)
         return (
           <tr key={s}>
             <th> {item[s].inventory.name} </th>
-            <th> {item[s].inventory.quantity} </th>
             <th> {item[s].inventory.description} </th>
+            <th> {item[s].inventory.quantity} </th>
             <th>
               <button className="btn btn-danger" value={item[s].inventory.uuid} onClick={self._handleOnClick.bind(self)}>Delete</button>
-              <button className="btn btn-info" value={item[s].inventory.uuid} onClick={self. _editFirebaseData.bind(self)}>Edit</button>
+              <button className="btn btn-info" value={item[s].inventory.uuid} onClick={self._setFireBaseDataEditTable}>Edit</button>
             </th>
           </tr>
         )
@@ -126,14 +213,14 @@ firebase.database().ref().child('/inventory-app/' + uuid2)
     });
 
     table = (
-      <div>
+      <div className="table-div">
         <Table striped bordered condensed hover>
           <thead>
-            <tr className="info">
-              <th> Name </th>
-              <th> Description </th>
-              <th> Quantity </th>
-              <th> Action </th>
+            <tr>
+              <th className="t-head"> Name </th>
+              <th className="t-head"> Description </th>
+              <th className="t-head"> Quantity </th>
+              <th className="t-head"> Action </th>
             </tr>
           </thead>
           <tbody>
@@ -143,23 +230,65 @@ firebase.database().ref().child('/inventory-app/' + uuid2)
       </div>
     )
 
-    output = (
+    editView = (
+      <div>
+        <h2 className="h2_1">Please Make Edits</h2>
+        <form className="form-inline" onSubmit={this._editFirebaseData}>
+          <input className="form-control" type="text" value={this.state.editFields.name} onChange={this.handleChange} name="name" />
+          <input className="form-control" type="text" value={this.state.editFields.description} onChange={this.handleChange} name="description" />
+          <input className="form-control" type="text" value={this.state.editFields.quantity} onChange={this.handleChange} name="quantity" />
+          <input className="form-control" type="text" className="hide input" value={this.state.editFields.uuid} name="uuid" />
+          <button className="btn btn-primary" type="submit" type="submit" >Submit</button>
+          <button className="btn btn-default" onClick={self._cancelFirebaseEdit}>Cancel</button>
+        </form>
+      </div>
+    );
+
+
+    if (this.state.editMode) {
+      output = (
+        <div className="App">
+          <div className="App-header">
+            <h2 className="h2_2">Coffee Inventory App</h2>
+            <ul className="nav nav-pills">
+              <li role="presentation" class="active"><a href="#">Home</a></li>
+              <li role="presentation"><a href="#">Inventory</a></li>
+              <li role="presentation"><a href="#">Edit Form</a></li>
+            </ul>
+            <img src="https://c1.staticflickr.com/4/3759/32940464915_43cd06aa2a_z.jpg" width="200" height="100" alt="coffee-cup-background-1 (1)" />
+          </div>
+          <div className="text-center">
+            {editView}
+          </div>
+        </div>
+      );
+    } else {
+      output = (
+        <div>
+          <div className="App-header">
+            <h2 className="h2_2">Coffee Inventory App</h2>
+            <ul className="nav nav-pills">
+              <li role="presentation"><a href="#">Home</a></li>
+              <li role="presentation"><a href="#">Inventory</a></li>
+              <li role="presnetation"><a href="#">Edit Form</a></li>
+              </ul>
+            <img src="https://c1.staticflickr.com/4/3759/32940464915_43cd06aa2a_z.jpg" width="200" height="100" alt="coffee-cup-background-1 (1)" />
+          </div>
+          <div className="text-center">
+            {inputForm}
+            {table}
+          </div>
+        </div>
+      );
+    }
+
+    return (
       <div className="App">
-        <div className="App-header">
-          <h2>Inventory App</h2>
-        </div>
-        <div className="text-center">
-          {inputForm}
-          <br />
-          {table}
-        </div>
+        {output}
       </div>
     );
   }
-    
-  return output;
-  }
-  
+
 
   //adding our function that will handle our submit
   onSubmit(event) {
@@ -191,7 +320,5 @@ firebase.database().ref().child('/inventory-app/' + uuid2)
     this._loadFirebaseData();
   }
 }
-
-
 
 export default App;
